9b262a0ee102218a09703bc4eccdddfe54c73ff5
iharashuto <iharashuto@gmail.com>
2020-03-07 10:10:04 +0000

    課題３　いいね機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :books
+  resources :favorites, only: [:create, :destroy]
   root 'books#top'
   get "home/about" => "books#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
